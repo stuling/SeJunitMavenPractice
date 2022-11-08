f3d1cd87e400046e5503925c9b710dee83f997cc
stuling <stuling@outlook.com>
2022-11-08 14:40:09 -0800

    adding LoginPage methods

--- src/test/java/sauce/pages/pom/pages/LoginPage.java
+++ src/test/java/sauce/pages/pom/pages/LoginPage.java
@@ -1,8 +1,12 @@
 package sauce.pages.pom.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import sauce.pages.pom.base.BasePage;
 
+/**
+ * Swaglabs login page
+ */
 public class LoginPage extends BasePage {
 
     public LoginPage(WebDriver driver) {
@@ -12,4 +16,23 @@ public class LoginPage extends BasePage {
     public String getTitle(){
         return driver.getTitle();
     }
+
+    public void enterUserName(String username) {
+
+        driver.findElement(By.id("user-name")).sendKeys(username);
+    }
+
+    /**
+     * Swaglabs login page has 1 password, secret_sauce
+     */
+    public void enterPassword() {
+
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+    }
+
+    public void clickLoginButton() {
+
+        driver.findElement(By.id("login-button")).click();
+    }
+
 }
