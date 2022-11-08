c542d1bc7860ec434179c6f1e53748c967c30c4f
stuling <stuling@outlook.com>
2022-11-08 14:40:46 -0800

    adding User enum for parameterized tests

---
+++ src/test/java/sauce/pages/User.java
@@ -0,0 +1,12 @@
+package sauce.pages;
+
+/**
+ * Swaglabs login page (https://saucedemo.com) provides
+ * 3 valid logins to log into the store page. This
+ * enum captures them.
+ */
+public enum User {
+    standard_user,
+    problem_user,
+    performance_glitch_user
+}
