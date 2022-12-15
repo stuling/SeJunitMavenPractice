8cf886f70bf3968f539e2df723e05a869b1f3233
stuling <stuling@outlook.com>
2022-12-14 16:41:49 -0800

    adding ProductPage dropdown test

--- src/test/java/sauce/pages/SwagProductsPageTest.java
+++ src/test/java/sauce/pages/SwagProductsPageTest.java
@@ -1,7 +1,11 @@
 package sauce.pages;
 
+import heroku.pages.pom.pages.DropDownPage;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import sauce.pages.pom.base.BaseTest;
 import sauce.pages.pom.pages.LoginPage;
 import sauce.pages.pom.pages.ProductsPage;
@@ -22,6 +26,28 @@ public class SwagProductsPageTest extends BaseTest {
 
         ProductsPage swagProductsPage = new ProductsPage(driver);
         assertEquals("Swag Labs", swagProductsPage.getTitle());
+    }
+
+    @Test
+    public void checkProductsPageDropDownHas4Values() {
+
+        driver.get("https://saucedemo.com/");
+        LoginPage swagLoginPage = new LoginPage(driver);
+
+        swagLoginPage.enterUserName("standard_user");
+        swagLoginPage.enterPassword();
+        swagLoginPage.clickLoginButton();
+
+        // find the dropdown element by className
+        WebElement dropdown = driver.findElement(By.className("product_sort_container"));
+
+        // pass the dropdown WebElement into a Select class
+        Select select = new Select(dropdown);
+
+        // get the size of the list
+        int dropdownSize = select.getOptions().size();
+
+        assertEquals(4, dropdownSize);
     }
 
     @Test
--- src/test/java/sauce/pages/pom/pages/ProductsPage.java
+++ src/test/java/sauce/pages/pom/pages/ProductsPage.java
@@ -3,6 +3,7 @@ package sauce.pages.pom.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import sauce.pages.pom.base.BasePage;
 
 public class ProductsPage extends BasePage {
@@ -24,5 +25,20 @@ public class ProductsPage extends BasePage {
         findShoppingCartLink().click();
     }
 
+    /**
+     * Find the dropdown option list and click on an option
+     * @param dropDownOption the name of the option to choose
+     * @return a Select class to work with after clicking
+     */
+    public Select clickAndChooseDropDownOption(String dropDownOption) {
+        // find the dropdown element by className
+        WebElement dropdown = driver.findElement(By.className("product_sort_container"));
+
+        // pass the dropdown WebElement into a Select class
+        Select select = new Select(dropdown);
+        select.selectByVisibleText(dropDownOption);
+
+        return select;
+    }
 
 }
